1aa070a70af724a20a127a6cbfaeedfd8a796169
Edgar Catalan <edgar@Edgars-MacBook-Pro.local>
2021-10-07 13:32:23 -0700

    Fixed Google Strategy proxy

--- services/passport.js
+++ services/passport.js
@@ -22,8 +22,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
       proxy: true,
+      callbackURL: "/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       // console.log(accessToken);

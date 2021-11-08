09af09209dc5293d1847d8bd17185cfb1f9208c0
Edgar Catalan <edgar@Edgars-MacBook-Pro.local>
2021-11-08 12:52:51 -0800

    fixed port issue

--- index.js
+++ index.js
@@ -45,5 +45,5 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.NODE_ENV || 5000;
-app.listen(PORT, () => console.log(`listening on port:${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
